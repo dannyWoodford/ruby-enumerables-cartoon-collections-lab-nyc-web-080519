a1503e7853e604e186b461bac8441fcebe9eec92
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-26 19:17:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,10 @@ end
 
 
 def long_planeteer_calls(array)
-  if array.any?{ |word| word.length > 4}
-    return true
-  elsif array.all?{ |word| word.length <= 4}
-    return false
+  if array.all?{ |word| word.length <= 4}
+    false
+  else 
+    true
   end
 end
 
